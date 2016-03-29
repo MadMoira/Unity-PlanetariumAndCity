f95aa5f79250f1c2cb28d32a7ca3324d596b5aeb
Maly <holiwords@gmail.com>
2016-03-29 07:40:17 -0500

    Add breackout game, casual mining and reestructure folders

---
+++ Assets/RollABall/_Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody sphereRigidBody;
+    private int count;
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        count = 0;
+        sphereRigidBody = GetComponent<Rigidbody>();
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+        sphereRigidBody.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 11)
+        {
+            winText.text = "You Win!!";
+        }
+    }
+
+}
